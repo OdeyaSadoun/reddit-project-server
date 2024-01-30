7d09eedb0ce036ca60aed4b25b4580ea74e002ba
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-30 10:40:17 +0200

    Add .gitattributes for line endings

---
+++ model/sentiments_model.py
@@ -0,0 +1,37 @@
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+
+
+def get_label(config, ranking):
+    highest_index = ranking[0]
+    return config.id2label[highest_index]
+
+
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+
+def sentiment_classify(text):
+    model = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    config = AutoConfig.from_pretrained(model)
+    model = AutoModelForSequenceClassification.from_pretrained(model)
+    text = preprocess(text)
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    ranking = np.argsort(scores)
+    ranking = ranking[::-1]
+    label = get_label(config, ranking)
+    return label
+
+
+print(f"Sentiment: {sentiment_classify('love')}")
