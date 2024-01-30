13affa49ab2a8c12c19dfd1e70e09262c08f4121
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-30 14:31:51 +0200

    fix session file

---
+++ db/session.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(expire_on_commit=False, bind=engine)
