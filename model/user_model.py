ad7f65f78a5e248f1be3bcbb9c0ed23f3591cacb
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-30 14:31:22 +0200

    add user model and user schema

---
+++ model/user_model.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from db.session import Base
+import datetime
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
