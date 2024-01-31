e66c099c879882d23c0a0e8c92f3c1fc132cd1ad
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-31 10:57:23 +0200

    change name to users controller

--- api/routes/user_route.py
+++ api/routes/user_route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from api.controllers import user_controller
+from api.controllers import users_controller
 from api.db.session import get_session
 from api.models import user_model
 from api.schemas import user_schema
@@ -14,14 +14,14 @@ jwt_bearer = auth_bearer.JWTBearer()
 
 @router.post("/register")
 def register(user: user_schema.UserSchemaCreate, session: Session = Depends(get_session)):
-    return user_controller.register_user(user, session)
+    return users_controller.register_user(user, session)
 
 
 @router.get('/getusers')
 def get_users_route(jwt_token: str = Depends(jwt_bearer), session: Session = Depends(get_session)):
-    return user_controller.get_users(session, jwt_token)
+    return users_controller.get_users(session, jwt_token)
 
 
 @router.get("/get_user_info", response_model=user_schema.UserSchemaResponse)
-def get_user_info(user: user_model.User = Depends(user_controller.get_user_from_token)):
+def get_user_info(user: user_model.User = Depends(users_controller.get_user_from_token)):
     return user
