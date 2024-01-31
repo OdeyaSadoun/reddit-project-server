21e5f939d406455424a33f7a9ef113cfdc4eb919
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-31 11:01:16 +0200

    change name to users route

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
 from api.db.session import engine, Base
-from api.routes import user_route, auth_route
+from api.routes import auth_route, users_route
 
 
 Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
 
-app.include_router(user_route.router, prefix="/users", tags=["users"])
+app.include_router(users_route.router, prefix="/users", tags=["users"])
 app.include_router(auth_route.router, prefix='/auth', tags=['auth'])
