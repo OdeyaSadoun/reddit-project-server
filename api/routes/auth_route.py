b90cd6928978cd0818d7077d4473f412a7ff28ae
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-31 11:00:51 +0200

    fix syntax in aut route

--- api/routes/auth_route.py
+++ api/routes/auth_route.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from api.controllers.auth_controller import login, logout
-from api.schemas import auth_schema, token_schema
+
+from api.controllers import auth_controller
 from api.db.session import get_session
-from api.utils.auth_bearer import JWTBearer
+from api.schemas import auth_schema, token_schema
+from api.utils import auth_bearer
 
 router = APIRouter()
-jwt_bearer = JWTBearer()
+jwt_bearer = auth_bearer.JWTBearer()
+
 
 @router.post('/login', response_model=token_schema.TokenSchemaResponse)
 def login_route(request: auth_schema.LoginSchema, db: Session = Depends(get_session)):
-    return login(request, db)
+    return auth_controller.login(request, db)
+
 
 @router.post('/logout')
 def logout_route(jwt_token: str = Depends(jwt_bearer), db: Session = Depends(get_session)):
-    return logout(jwt_token, db)
+    return auth_controller.logout(jwt_token, db)
