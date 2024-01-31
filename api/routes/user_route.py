31dc2637193e24c8256a5794f2c2166424f76bf2
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-31 10:56:02 +0200

    user route- fix syntax

--- api/routes/user_route.py
+++ api/routes/user_route.py
@@ -1,27 +1,27 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from api.controllers.user_controller import register_user, get_users, get_user_from_token
+from api.controllers import user_controller
 from api.db.session import get_session
-from api.utils.auth_bearer import JWTBearer
-from api.schemas import user_schema, auth_schema
 from api.models import user_model
+from api.schemas import user_schema
+from api.utils import auth_bearer
 
 
 router = APIRouter()
-jwt_bearer = JWTBearer()
+jwt_bearer = auth_bearer.JWTBearer()
 
 
 @router.post("/register")
 def register(user: user_schema.UserSchemaCreate, session: Session = Depends(get_session)):
-    return register_user(user, session)
+    return user_controller.register_user(user, session)
 
 
 @router.get('/getusers')
 def get_users_route(jwt_token: str = Depends(jwt_bearer), session: Session = Depends(get_session)):
-    return get_users(session, jwt_token)
+    return user_controller.get_users(session, jwt_token)
 
 
 @router.get("/get_user_info", response_model=user_schema.UserSchemaResponse)
-def get_user_info(user: user_model.User = Depends(get_user_from_token)):
+def get_user_info(user: user_model.User = Depends(user_controller.get_user_from_token)):
     return user
