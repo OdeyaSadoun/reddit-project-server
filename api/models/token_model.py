ac1e08f6e78300ae03ee9bc8379fe1d27473d82d
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-01 15:28:02 +0200

    reformat token model

--- api/models/token_model.py
+++ api/models/token_model.py
@@ -3,11 +3,12 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean
 
 from api.db.session import Base
 
+
 class TokenTable(Base):
     __tablename__ = "tokens"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False)
     access_token = Column(String(450), unique=True, nullable=False)
-    refresh_token = Column(String(450),nullable=False)
+    refresh_token = Column(String(450), nullable=False)
     status = Column(Boolean)
     created_date = Column(DateTime, default=datetime.datetime.now)
