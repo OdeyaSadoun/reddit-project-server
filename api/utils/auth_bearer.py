acf72ee7ce36a6746e59bef555d6d46f4b4fe649
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-01 15:30:40 +0200

    reformat auth bearer

--- api/utils/auth_bearer.py
+++ api/utils/auth_bearer.py
@@ -5,15 +5,14 @@ from pathlib import Path
 
 from api.models import jwt_bearer_model
 
-
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-REFRESH_TOKEN_EXPIRE_MINUTES : int = eval(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
-ALGORITHM : str = os.getenv("ALGORITHM")
-JWT_SECRET_KEY : str = os.getenv("JWT_SECRET_KEY")
-JWT_REFRESH_SECRET_KEY : str = os.getenv("JWT_REFRESH_SECRET_KEY")
+REFRESH_TOKEN_EXPIRE_MINUTES: int = eval(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
+ALGORITHM: str = os.getenv("ALGORITHM")
+JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+JWT_REFRESH_SECRET_KEY: str = os.getenv("JWT_REFRESH_SECRET_KEY")
 
 
 def decodeJWT(jwtoken: str):
@@ -23,5 +22,6 @@ def decodeJWT(jwtoken: str):
         return payload
     except jwt.PyJWTError:
         return None
+
 
 jwt_bearer = jwt_bearer_model.JWTBearer()
