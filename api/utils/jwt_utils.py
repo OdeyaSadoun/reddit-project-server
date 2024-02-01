704411f8551eadba981908cd149f8704ab698558
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-01 15:31:05 +0200

    reformat utiles

--- api/utils/jwt_utils.py
+++ api/utils/jwt_utils.py
@@ -10,10 +10,10 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-REFRESH_TOKEN_EXPIRE_MINUTES : int = eval(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
-ALGORITHM : str = os.getenv("ALGORITHM")
-JWT_SECRET_KEY : str = os.getenv("JWT_SECRET_KEY")
-JWT_REFRESH_SECRET_KEY : str = os.getenv("JWT_REFRESH_SECRET_KEY")
+REFRESH_TOKEN_EXPIRE_MINUTES: int = eval(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
+ALGORITHM: str = os.getenv("ALGORITHM")
+JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+JWT_REFRESH_SECRET_KEY: str = os.getenv("JWT_REFRESH_SECRET_KEY")
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -29,13 +29,13 @@ def verify_password(password: str, hashed_pass: str) -> bool:
 def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
     if expires_delta is not None:
         expires_delta = datetime.utcnow() + expires_delta
-        
+
     else:
         expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-           
+
     to_encode = {"exp": expires_delta, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
-     
+
     return encoded_jwt
 
 
@@ -44,7 +44,7 @@ def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) ->
         expires_delta = datetime.utcnow() + expires_delta
     else:
         expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
-    
+
     to_encode = {"exp": expires_delta, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
     return encoded_jwt
