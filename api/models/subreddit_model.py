d4d9ed9ddc41643d82cbc194339d21028fdbc1c4
אודיה סעדון <osadoun@g.jct.ac.il>
2024-02-14 07:50:58 +0200

    add new files to exceptions

--- api/models/subreddit_model.py
+++ api/models/subreddit_model.py
@@ -11,7 +11,7 @@ class SubredditSearch(Base):
     __tablename__ = 'subredditsearches'
     id = Column(Integer, primary_key=True, index=True)
     reddit_id = Column(Integer, nullable=False)
-    title = Column(String(100), nullable=False, index=True)
+    title = Column(String(1000), nullable=False, index=True)
     selftext = Column(Text, nullable=False, index=True)
     sentiment = Column(ENUM(ModelRedditSentiment), nullable=False, index=True)
     ups = Column(Integer, nullable=False, index=True)
